fd4c73c63391c1c93d0bbf22cad71ee8f836efe2
Dauren Abasov <daurenabasov987@gmail.com>
2022-09-27 23:32:22 +0600

    feat: add modal

--- src/pages/Recipients.jsx
+++ src/pages/Recipients.jsx
@@ -1,88 +1,69 @@
-import React from "react";
-import cl from "../scss/recipients.module.scss";
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import cl from "../scss/recipients.module.scss";;
 
 const Recipients = () => {
-  return <div>
-    <section className={cl.recipients__container}>
-      <div className={cl.recipients__content}>
-        <h2>
-          ФИО залогодателя
-        </h2>
-        <input
-          className={cl.recipients__input}
-          type="text"
-        />
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-        <h2>
-          Семейное положение:
-        </h2>
-        <select id={cl.recipients__accor}>
-          <option value="Выбрать" >Выбрать</option>
-        </select>
-        <h2>
-          Credit history
-        </h2>
-        <input
-          type="file"
-        />
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
-        <h2>
-          Номер телефона:
-        </h2>
-        <input
-          className={cl.recipients__input}
-          type="number"
-        />
-        <h2>
-          Адрес прописки:
-        </h2>
-        <input
-          className={cl.recipients__input}
-          type="text"
-        />
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
 
-        <h2>
-          Адрес фактический:
-        </h2>
-        <input
-          className={cl.recipients__input}
-          type="number"
-        />
-        <h2>
-          icome startement:
-        </h2>
-        <input
-          
-          type="file" />
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
-        <h2>
-          Залоговое имущество:
-        </h2>
-        <input
-          className={cl.recipients__input}
-          type="text" />
-        <h2>
-          Местонахождение залога:
-        </h2>
+  return (
+    <div>
+      <section className={cl.recipients__container}>
+        <div className={cl.recipients__content}>
+          <h2>ФИО залогодателя</h2>
+          <input className={cl.recipients__input} type="text" />
 
-        <input
-          className={cl.recipients__input}
-          type="text" />
-        <h2>
-          Document
-        </h2>
-        <input
-          
-          type="file" />
+          <h2>Семейное положение:</h2>
+          <select id={cl.recipients__accor}>
+            <option value="Выбрать">Выбрать</option>
+          </select>
+          <h2>Credit history</h2>
+          <input type="file" />
 
-      </div>
-      <button className={cl.recipients__btn}>
-        Submit
-      </button>
+          <h2>Номер телефона:</h2>
+          <input className={cl.recipients__input} type="number" />
+          <h2>Адрес прописки:</h2>
+          <input className={cl.recipients__input} type="text" />
 
-    </section>
+          <h2>Адрес фактический:</h2>
+          <input className={cl.recipients__input} type="number" />
+          <h2>icome startement:</h2>
+          <input type="file" />
 
-  </div>;
+          <h2>Залоговое имущество:</h2>
+          <input className={cl.recipients__input} type="text" />
+          <h2>Местонахождение залога:</h2>
+
+          <input className={cl.recipients__input} type="text" />
+          <h2>Document</h2>
+          <input type="file" />
+        </div>
+        <Button type="primary" onClick={showModal} style={{
+          background: "#42B4F4", width: "324px", height: "63px", fontSize: "20px"
+        }} >
+          Open Modal
+        </Button>
+        <Modal
+          title="Basic Modal"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        ><h1>
+            Ваш залог принят</h1></Modal>
+      </section>
+    </div>
+  );
 };
 
 export default Recipients;
