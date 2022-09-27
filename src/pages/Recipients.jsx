e5c327791e222fab4f2d2cdd1d90edac3ef0a168
Dauren Abasov <daurenabasov987@gmail.com>
2022-09-27 22:42:07 +0600

    feat: add recipients page

--- src/pages/Recipients.jsx
+++ src/pages/Recipients.jsx
@@ -2,7 +2,87 @@ import React from "react";
 import cl from "../scss/recipients.module.scss";
 
 const Recipients = () => {
-  return <div>Поручители</div>;
+  return <div>
+    <section className={cl.recipients__container}>
+      <div className={cl.recipients__content}>
+        <h2>
+          ФИО залогодателя
+        </h2>
+        <input
+          className={cl.recipients__input}
+          type="text"
+        />
+
+        <h2>
+          Семейное положение:
+        </h2>
+        <select id={cl.recipients__accor}>
+          <option value="Выбрать" >Выбрать</option>
+        </select>
+        <h2>
+          Credit history
+        </h2>
+        <input
+          type="file"
+        />
+
+        <h2>
+          Номер телефона:
+        </h2>
+        <input
+          className={cl.recipients__input}
+          type="number"
+        />
+        <h2>
+          Адрес прописки:
+        </h2>
+        <input
+          className={cl.recipients__input}
+          type="text"
+        />
+
+        <h2>
+          Адрес фактический:
+        </h2>
+        <input
+          className={cl.recipients__input}
+          type="number"
+        />
+        <h2>
+          icome startement:
+        </h2>
+        <input
+          
+          type="file" />
+
+        <h2>
+          Залоговое имущество:
+        </h2>
+        <input
+          className={cl.recipients__input}
+          type="text" />
+        <h2>
+          Местонахождение залога:
+        </h2>
+
+        <input
+          className={cl.recipients__input}
+          type="text" />
+        <h2>
+          Document
+        </h2>
+        <input
+          
+          type="file" />
+
+      </div>
+      <button className={cl.recipients__btn}>
+        Submit
+      </button>
+
+    </section>
+
+  </div>;
 };
 
 export default Recipients;
