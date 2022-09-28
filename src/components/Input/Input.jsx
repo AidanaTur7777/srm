3ffe517a55b350a277dad21e2694b31d8d292f25
Dauren Abasov <daurenabasov987@gmail.com>
2022-09-28 21:48:05 +0600

    fix: fixed adaptive

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cl from "./input.module.scss";
 
-const Input = ({height="60px",width="482px"}) => {
+const Input = ({ height = "60px", width = "482px" }) => {
   return (
     <input
       type="text"
