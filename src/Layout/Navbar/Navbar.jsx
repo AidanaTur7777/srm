8e99d2be3b7e93007790cf9b73f7bf0a9828e385
unknown <dauren14a@gmail.com>
2022-10-03 19:15:26 +0600

    Added navbar list changes

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { SidebarContext } from '../../context'
 import cl from './Navbar.module.scss'
 import { MdOutlineMenu } from 'react-icons/md'
+import { useLocation } from 'react-router'
 
 const Navbar = () => {
   const { sidebarOpen, setSidebarOpen } = useContext(SidebarContext)
+  const location=useLocation();
+  const [list,setList]=useState("Документы на КК");
+  useEffect(()=>{
+    if(location.pathname=="/documents"){
+      setList("Документы на КК")
+    }else if(location.pathname=="/positions"){
+      setList("Должности")
+    }else if(location.pathname=="/companies"){
+      setList("Компании")
+    }else if(location.pathname=="/counterparties"){
+      setList("Контрагенты")
+    }else if(location.pathname=="/recipients"){
+      setList("Поручители")
+    }else if(location.pathname=="/conversations"){
+      setList("Тел. переговоры")
+    }else{
+      setList("Документы на КК")
+    }
+  },[])
   return (
     <div className={cl.navbar}>
       <h2 className={cl.navbar__left}>
@@ -14,7 +34,7 @@ const Navbar = () => {
         />
         <span>Dashboard</span>
       </h2>
-      <h2 className={cl.navbar__right}>Dashboard / Data KK / List</h2>
+      <h2 className={cl.navbar__right}>Dashboard / {list}</h2>
     </div>
   )
 }
